c66fd6a3f334ff77bc4fc642a3e24d98a5db9a82
LurkePoint <49284978+LurkePoint@users.noreply.github.com>
2021-05-24 17:20:29 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    $('.burger_menu').click(function() {
+        $('.burger_menu,.menu').toggleClass('active');
+        $('.block_submenu').removeClass('active');
+        $('body').toggleClass('lock');
+    });
+
+    $('.menu_tasks').click(function() {
+        $('.block_submenu').addClass('active');
+    });
+
+    $('.submenu_tasks_back').click(function() {
+        $('.block_submenu').removeClass('active');
+    });
+
+    $('.title_description').click(function() {
+        $(this).closest('.selector_description').children('.block_description').slideToggle({duration: 300});
+        $(this).children('.arrow_description').toggleClass('rotate');
+    });
+
+    $('.block_top').on('click', function() {
+        $('html, body').animate({
+          scrollTop: 0,
+        }, 500);
+    });
+
+    $(window).scroll(function() {
+        const height = $(window).scrollTop(); 
+        if(height > 200){
+            $('.block_top').addClass('visible');
+        } else {
+            $('.block_top').removeClass('visible');
+        }
+    })
+});
+
